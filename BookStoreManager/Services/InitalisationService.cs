a195660930ae10a96c0cac6766f152b53b37f921
sharansudheer <sharan.sharansudheer@gmail.com>
2025-07-01 16:35:33 +0530

    push:tag101
    Initalisation Service

---
+++ BookStoreManager/Services/InitalisationService.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStoreManager.Services
+{
+ // Dependency Injection(DUI)
+ // Business Class
+ // Initalise The database, cache services, tables which are predefined
+ // Add Grout 
+ // Check Initalisation Process and Flag yes or no for status updation
+ // Error and bypass circuits
+ // Log and verbose(if necessary) and make a log file(during the last stage of dev)
+ // Opt - A terminal window for manual intervention
+    public class InitalisationService
+    {
+    }
+}
